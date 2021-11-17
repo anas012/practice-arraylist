91b05d1e283d4067335273fc657ab1a589677503
anas012 <anas@salesassist.pk>
2021-11-17 23:41:05 +0500

    add a new frnd to list

--- app/src/main/java/com/example/arraylist/MainActivity.java
+++ app/src/main/java/com/example/arraylist/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.arraylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,15 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button btn=findViewById(R.id.addnamebtn);
+        EditText name=findViewById(R.id.plaintext1);
         ListView list=findViewById(R.id.testlist);
         ArrayList<String>friendslist=new ArrayList<String >();
-        friendslist.add("Fama");
-        friendslist.add("Mujtaba");
-        friendslist.add("Areeb");
-        friendslist.add("Arslan");
+        friendslist.add("frnd1");
+        friendslist.add("frnd2");
+        friendslist.add("frnd3");
+        friendslist.add("frnd4");
         ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String >(this,android.R.layout.simple_list_item_1,friendslist);
         list.setAdapter(arrayAdapter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String a=name.getText().toString();
+            friendslist.add(a);
+            arrayAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
 }
