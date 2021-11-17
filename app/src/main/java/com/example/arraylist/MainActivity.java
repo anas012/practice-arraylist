7d908c90872bc02801ce3354ddf89c54c0b732c6
anas012 <anas@salesassist.pk>
2021-11-17 23:27:39 +0500

    list view created

--- app/src/main/java/com/example/arraylist/MainActivity.java
+++ app/src/main/java/com/example/arraylist/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.arraylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list=findViewById(R.id.testlist);
+        ArrayList<String>friendslist=new ArrayList<String >();
+        friendslist.add("Fama");
+        friendslist.add("Mujtaba");
+        friendslist.add("Areeb");
+        friendslist.add("Arslan");
+        ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String >(this,android.R.layout.simple_list_item_1,friendslist);
+        list.setAdapter(arrayAdapter);
     }
+
 }
